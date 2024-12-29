c8da88eb790c16751b2977550c64e6d573dd3a7d
zfq <zhangfengqianbupt@bupt.edu.cn>
2024-12-29 15:20:45 +0800

    feat(DNSName): 增加 DNS 名称的可读性转换方法
    # DETAILS:
    - 在 DNSLabel 类中添加 ToReadableString 方法，用于将标签转换为可读字符串
    - 在 DNSName 类中添加 ToReadableString 方法，用于将整个 DNS 名称转换为可读字符串
    - 新增方法能够正确处理特殊字符，如点号和反斜杠，并将其他非可打印字符转换为转义形式
    - 上述实现方法的实现均参考了 pdns 项目中的 `dnsname.hh` 或 `dnsname.cc` 文件中的具体实现

--- TDNS/Entity/DNSName.cs
+++ TDNS/Entity/DNSName.cs
@@ -1,9 +1,44 @@
-﻿namespace TDNS.DNSStorage;
+﻿using System.Text;
+
+namespace TDNS.DNSStorage;
 
 public class DNSLabel
 {
     public Byte[] label;
     public DNSLabel(Byte[] label) { this.label = label; }
+    
+    private const Byte PointByte = (Byte)'.',
+        EndByte = (Byte)'\0',
+        // 这个 Backslash 是字符名称, 但是不少项目使用的状态名称含有 `Escape`, 其实指的就是 `Escape Characters` 转义字符
+        BackslashByte = (Byte)'\\';
+
+    internal void ToReadableString(StringBuilder sb)
+    {
+        foreach (var b in label)
+        {
+            switch (b)
+            {
+                case PointByte:
+                    sb.Append("\\.");
+                    break;
+                case BackslashByte:
+                    sb.Append(@"\\");
+                    break;
+                case > 0x20 and < 0x7f:
+                    sb.Append((char)b);
+                    break;
+                default:
+                {
+                    var buffer = b.ToString("D3");
+                    if (buffer.Length > 3)
+                        throw new InvalidOperationException("DNS Label 存在转移之后长度大于 3 的字符串");
+                    sb.Append('\\').Append(buffer);
+                    break;
+                }
+            }
+        }
+        
+    }
 }
 
 public class DNSName
@@ -15,4 +50,16 @@ public class DNSName
     
     public static DNSName operator +(DNSName lhs, DNSName rhs) { lhs.labels.AddRange(rhs.labels); return lhs; }
     // @fmt:on
+
+    public String ToReadableString()
+    {
+        var sb = new StringBuilder();
+        foreach (var label in labels)
+        {
+            label.ToReadableString(sb);
+            sb.Append('.');
+        }
+
+        return sb.ToString();
+    }
 }
